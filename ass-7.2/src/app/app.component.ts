2a0f421bbca5741bb06f45b42e96a25f2e45dd46
jasmitha <jasmieyjas@gmail.com>
2017-12-04 23:41:05 +0530

    Add files via upload

---
+++ ass-7.2/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+// defining component selector.templateUrl and styleUrls.
+@Component({
+  selector: 'app-component',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.css']
+})
+ 
+export class RootComponent {
+
+  cricketPlayer: ICricketPlayer[] = [];
+  ngOnInit() {
+    this.cricketPlayer = [{
+      name: "Sachin",
+      lastName: 'Tendulkar'
+    },
+    {
+      name: "MS",
+      lastName: 'Dhoni'
+    },
+    {
+      name: "Virat",
+      lastName: 'Kohli'
+    }]
+
+  }
+}
+// defining interface ICricketPlayer
+interface ICricketPlayer {
+  name: string;
+  lastName: string;
+}
